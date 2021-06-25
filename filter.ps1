83f3f753b776c6262b5b20cbf8f61b7d4e0a1a4e
LogCreative <longlaoda123456@qq.com>
2021-06-25 11:20:49 +0800

    book

--- filter.ps1
+++ filter.ps1
@@ -2,12 +2,12 @@ Get-ChildItem . '*.tex' -Force -Recurse | ForEach-Object -Process {
     $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
 
     (Get-Content $file)|
-        ForEach-Object { $_ -replace 'Zilong Li','Log Creative'} |  # Change Name
-        ForEach-Object { $_ -replace '518070910095',''} | # Change Number
+        ForEach-Object { $_ -replace '***********','Log Creative'} |  # Change Name
+        ForEach-Object { $_ -replace '************',''} | # Change Number
         Out-File $file -Force
 }
 
-Get-ChildItem .\ -recurse *ZilongLi* | ForEach-Object {
+Get-ChildItem .\ -recurse ************* | ForEach-Object {
     $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
     Remove-Item $file
 }
